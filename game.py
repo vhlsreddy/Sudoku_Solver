558484d69cebe3aa62f4220289b15168007ef5a1
Vutukuru Hema Lakshmi Sandhya <113281309+vhlsreddy@users.noreply.github.com>
2023-07-05 15:06:17 +0530

    Add files via upload

---
+++ game.py
@@ -0,0 +1,102 @@
+board = [
+    [7,8,0,4,0,0,1,2,0],
+    [6,0,0,0,7,5,0,0,9],
+    [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
+    [0,0,1,0,5,0,9,3,0],
+    [9,0,4,0,6,0,0,0,5],
+    [0,7,0,3,0,0,0,1,2],
+    [1,2,0,0,0,7,4,0,0],
+    [0,4,9,2,0,6,0,0,7]
+]
+# This Method is comprised of following steps
+# 1.pick empty box....which means the box with value zero(here proceeding towards left)
+# 2.try all numbers(from 1 to 9 in that box)
+# 3.find one that fits
+# 4.repeat(recursion)
+# 5.sol cannot complete then backtrack( if the number is invaid then backtrack and proceed)
+
+
+#...........................Printing the given 2D matrix in the required board form.............
+
+def print_board(bo):
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(bo[i][j])
+            else:
+                print(str(bo[i][j]) + " ", end="")
+
+
+#..........................Finding the empty box..............
+
+def find_empty(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j] == 0:
+                return (i, j)  # row, col
+
+    return None
+
+
+
+
+#...........checking validity in terms of row, column, and box.............
+
+def valid(bo, num, pos):
+    # Check row
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # Check column
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # Check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x * 3, box_x*3 + 3):
+            if bo[i][j] == num and (i,j) != pos:
+                return False
+
+    return True
+
+
+#.......................solving it includes recursion and backtracking..................
+
+def solve(bo):
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1,10):
+        if valid(bo, i, (row, col)):
+            bo[row][col] = i
+
+            if solve(bo):
+                return True
+
+            bo[row][col] = 0
+
+    return False
+
+
+
+
+print_board(board)                                 #It prints the board before solving
+           
+solve(board)                                       #calling the function for solving
+print("___________________")
+print_board(board)                                 #It prints the board after solving
